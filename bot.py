5c5962e1ce68130990c27f5c774d60f893b30c25
smarttreader904-blip <smarttreader904@gmail.com>
2026-06-26 19:26:06 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,45 @@
-# Main bot runner
+# bot.py
 
-# Add your bot token and start polling here.
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import BOT_TOKEN
+from handlers import router
+from database import init_db
+
+
+async def main():
+
+    # Database Create
+    init_db()
+
+    # Bot Initialize
+    bot = Bot(BOT_TOKEN)
+
+    # Dispatcher
+    dp = Dispatcher(
+        storage=MemoryStorage()
+    )
+
+    # Include Router
+    dp.include_router(router)
+
+    print("✅ Bot Started Successfully!")
+
+    # Remove old updates
+    await bot.delete_webhook(
+        drop_pending_updates=True
+    )
+
+    # Start Bot
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+
+    except KeyboardInterrupt:
+        print("❌ Bot Stopped")
